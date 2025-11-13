7aafb924dff498dce115164e43df7179a9596e89
jjjwelch420 <RccsdChatAW@outlook.com>
2025-11-12 22:20:00 -0500

    Clean Up and Removal

---
+++ list.js
@@ -0,0 +1,17 @@
+const ROOT_URL = 'https://jjjwelch420.github.io/Launcher/';
+    const games = [
+      { id: 'racing', label: 'Racing' },
+      { id: 'stick-war', label: 'Stick War' },
+      { id: 'ARC', label: 'ARC' },
+      { id: 'sprinter-flash', label: 'Sprinter Flash' },
+      { id: 'minecraft', label: 'Minecraft' },
+      { id: 'gunspin', label: 'Gun Spin' },
+      { id: 'OvO', label: 'OvO' },
+      { id: 'Cookie', label: 'Cookie Clicker' },
+      { id: 'SM64', label: 'Super Ah 64' },
+      { id: 'Drift-RU', label: 'Drift Hunters' },
+      { id: 'Funny2', label: 'Funny Shooter' },
+      { id: 'snow-riders', label: 'Snow Riders' },
+      { id: 'riddle-1', label: 'Riddle School 1' },
+      { id: 'red-vs-blue', label: 'Red vs Blue' }
+    ];
