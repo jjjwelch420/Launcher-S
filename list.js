e74dd1b75dcb284809952d0254afb178a42e9d2e
jjjwelch420 <RccsdChatAW@outlook.com>
2025-11-14 16:34:02 -0500

    Genral Cleanup and Added JackSmith

--- list.js
+++ list.js
@@ -1,18 +1,19 @@
 const ROOT_URL = 'https://jjjwelch420.github.io/Launcher/';
     const games = [
-      { id: 'racing', label: 'Racing' },
-      { id: 'stick-war', label: 'Stick War' },
+      { id: 'Racing', label: 'Racing' },
+      { id: 'StickWar', label: 'Stick War' },
       { id: 'ARC', label: 'ARC' },
-      { id: 'sprinter-flash', label: 'Sprinter Flash' },
-      { id: 'minecraft', label: 'Minecraft' },
-      { id: 'gunspin', label: 'Gun Spin' },
+      { id: 'SprinterFlash', label: 'Sprinter Flash' },
+      { id: 'Minecraft', label: 'Minecraft' },
+      { id: 'GunSpin', label: 'Gun Spin' },
       { id: 'OvO', label: 'OvO' },
       { id: 'Cookie', label: 'Cookie Clicker' },
       { id: 'SM64', label: 'Super Ah 64' }, 
       { id: 'Drift-RU', label: 'Drift Hunters' },
       { id: 'Funny2', label: 'Funny Shooter' },
-      { id: 'snow-riders', label: 'Snow Riders' },
-      { id: 'riddle-1', label: 'Riddle School 1' },
+      { id: 'SnowRiders', label: 'Snow Riders' },
+      { id: 'Riddle-1', label: 'Riddle School 1' },
       { id: 'Learn2Fly', label: 'Learn to Fly 2' },
-      { id: 'red-vs-blue', label: 'Red vs Blue' }
+      { id: 'Red-vs-Blue', label: 'Red vs Blue' },
+      { id: 'JackSmith', label: 'JackSmith' }
     ];
