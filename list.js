47ee6557023362c387d5610b8c5191091423c51f
jjjwelch420 <RccsdChatAW@outlook.com>
2025-11-17 12:41:40 -0500

    Update list.js

--- list.js
+++ list.js
@@ -1,6 +1,6 @@
 const ROOT_URL = 'https://jjjwelch420.github.io/Launcher-S/';
     const games = [
-      { id: 'Racing', label: 'Racing' },
+      { id: 'Racing', label: 'Racing' }, 
       { id: 'StickWar', label: 'Stick War' },
       { id: 'ARC', label: 'ARC' },
       { id: 'SprinterFlash', label: 'Sprinter Flash' },
